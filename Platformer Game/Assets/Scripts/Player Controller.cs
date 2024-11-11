bb339f72c012942a3c6ca143148310c54f67c14c
Vortex111903 <03darianacosta@gmail.com>
2024-11-11 16:20:19 -0500

    movement added

--- Platformer Game/Assets/Scripts/Player Controller.cs
+++ Platformer Game/Assets/Scripts/Player Controller.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    private SpriteRenderer sr;
+
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    public Sprite frontSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = transform.position;
+
+        if (Input.GetKey("a")) 
+        {
+            newPosition.x -= speed;
+        }
+        if (Input.GetKey("d"))
+        {
+            newPosition.x += speed;
+        }
+        if (Input.GetKey("w"))
+        {
+            newPosition.y += speed;
+        }
     }
 }
